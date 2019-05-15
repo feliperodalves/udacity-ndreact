957b10058698acf9cdb42faff8f0446525b90c8d
Felipe Alves <felipe.rod.alves@gmail.com>
2019-05-15 19:28:58 -0300

    Ending of React Fundamentals Module

--- 001-react-fundamentals/reactnd-contacts/src/App.js
+++ 001-react-fundamentals/reactnd-contacts/src/App.js
@@ -1,33 +1,57 @@
-import React, { Component } from 'react';
-import ListContacts from './ListContacts';
-import * as ContactsAPI from './utils/ContactsAPI'
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import ListContacts from "./ListContacts";
+import CreateContact from "./CreateContact";
+import * as ContactsAPI from "./utils/ContactsAPI";
 
 class App extends Component {
   state = {
+    screen: "list",
     contacts: []
-  }
+  };
 
   componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
+    ContactsAPI.getAll().then(contacts => {
+      this.setState({ contacts });
+    });
+  }
+
+  removeContact = contact => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(c => c.id !== contact.id)
+    }));
+
+    ContactsAPI.remove(contact);
+  };
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
     })
   }
-
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter(c => c.id !== contact.id)
-    }))  
-
-    ContactsAPI.remove(contact)
-  }
-  
 
   render() {
     return (
       <div>
-        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+        <Route exact path="/" render={() => (
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.removeContact}
+            />
+          )}/>
+        <Route path="/create" render={({ history }) => (
+            <CreateContact
+              onCreateContact={(contact) => {
+                this.createContact(contact)
+                history.push('/')
+              }}
+            />
+        )}
+        />
       </div>
-    )
+    );
   }
 }
 
